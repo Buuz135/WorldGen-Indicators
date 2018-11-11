5eb9d80dbb200e266f1aa4739b34eaeffc3acad2
Buuz135 <buuz135@hotmail.com>
2018-11-11 16:22:50 +0100

    Improved chunk checking a lot

--- src/main/java/com/buuz135/worldgenindicators/WorldGenIndicators.java
+++ src/main/java/com/buuz135/worldgenindicators/WorldGenIndicators.java
@@ -31,15 +31,20 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Mod(
         modid = WorldGenIndicators.MOD_ID,
         name = WorldGenIndicators.MOD_NAME,
         version = WorldGenIndicators.VERSION
 )
+@Mod.EventBusSubscriber
 public class WorldGenIndicators {
 
     //TODO Block Dependents: Surface, Block Relative, Surface Relative
@@ -79,41 +84,64 @@ public class WorldGenIndicators {
 
     }
 
+    public static ConcurrentHashMap<Pair<World, BlockPos>, IChecker> CACHED_CHECKER = new ConcurrentHashMap<>();
+
+    @SubscribeEvent
+    public static void onServerTick(TickEvent.ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            CACHED_CHECKER.keySet().iterator().forEachRemaining(worldBlockPosPair -> {
+                CACHED_CHECKER.get(worldBlockPosPair).getRandomIndicator(worldBlockPosPair.getLeft().rand).generate(worldBlockPosPair.getLeft(), worldBlockPosPair.getRight());
+                CACHED_CHECKER.remove(worldBlockPosPair);
+            });
+        }
+    }
+
     public static class WorldGenIndicatorsGenerator implements IWorldGenerator {
 
-        @Override
-        public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-            for (int x = 8; x < 8 + 16; ++x) {
-                for (int z = 8; z < 8 + 16; ++z) {
-                    for (int i = 0; i < world.getTopSolidOrLiquidBlock(new BlockPos(chunkX * 16 + x, i, chunkZ * 16 + z)).getY() + 4; i++) {
-                        if (check(world, chunkX, chunkZ, x, z, i)) {
-                            break;
+        public static boolean started = false;
+        public static ConcurrentHashMap<Pair<Integer, Integer>, World> CHUNKS_TO_CHECK = new ConcurrentHashMap<>();
+        public static Thread THREAD = new Thread(() -> {
+            while (true) {
+                CHUNKS_TO_CHECK.keySet().iterator().forEachRemaining(integerIntegerPair -> {
+                    World world = CHUNKS_TO_CHECK.get(integerIntegerPair);
+                    int chunkX = integerIntegerPair.getLeft();
+                    int chunkZ = integerIntegerPair.getRight();
+                    for (int x = 8; x < 8 + 16; ++x) {
+                        for (int z = 8; z < 8 + 16; ++z) {
+                            for (int i = 0; i < world.getTopSolidOrLiquidBlock(new BlockPos(chunkX * 16 + x, i, chunkZ * 16 + z)).getY() + 4; i++) {
+                                check(world, chunkX, chunkZ, x, z, i);
+                            }
                         }
                     }
+                    CHUNKS_TO_CHECK.remove(integerIntegerPair);
+                });
+                try {
+                    Thread.sleep(25);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-//            for (int x = 0; x < 16; ++x) {
-//                for (int z = 0; z < 16; ++z) {
-//                    if (x == 0 && !chunkProvider.isChunkGeneratedAt(chunkX-1, chunkZ)) continue;
-//                    if (z == 0 && !chunkProvider.isChunkGeneratedAt(chunkX, chunkZ-1)) continue;
-//                    for (int i = 0; i < world.getTopSolidOrLiquidBlock(new BlockPos(chunkX * 16 + x, i, chunkZ * 16 + z)).getY() + 4; i++) {
-//                        if (check(world, chunkX, chunkZ, x, z, i)) {
-//                            break;
-//                        }
-//                    }
-//                }
-//            }
-        }
+        });
 
-        private boolean check(World world, int chunkX, int chunkZ, int x, int z, int i) {
+        private static boolean check(World world, int chunkX, int chunkZ, int x, int z, int i) {
             BlockPos pos = new BlockPos(chunkX * 16 + x, i, chunkZ * 16 + z);
             for (IChecker iChecker : WorldGenManager.checkerList) {
                 if (iChecker.isValid(world, pos) && world.rand.nextDouble() < iChecker.getWorkingChance()) {
-                    iChecker.getRandomIndicator(world.rand).generate(world, pos);
+                    CACHED_CHECKER.put(Pair.of(world, pos), iChecker);
                     return true;
                 }
             }
             return false;
+        }
+
+        @Override
+        public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+            if (!started) {
+                THREAD.start();
+                started = true;
+            }
+            if (!CHUNKS_TO_CHECK.containsKey(Pair.of(chunkX, chunkZ)))
+                CHUNKS_TO_CHECK.put(Pair.of(chunkX, chunkZ), world);
         }
     }
 }
